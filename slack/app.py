3d4e39819ef4a795459230514911ae4f58ed89a9
yunozon <guru.fu.kamie21@gmail.com>
2024-11-12 10:50:23 +0900

    first commit

---
+++ slack/app.py
@@ -0,0 +1,7 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
